555a07fe2144b56f5629c91d141aee5771c9be77
Konstantin Loginov <loginov.k@gmail.com>
2019-12-05 13:11:35 +0100

    keep searching for the solution

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JTCalendar",
-            dependencies: [],
+            path: "Sources",
+            exclude: [],
+            sources: ["JTCalendar", "JTCalendar/JTCalendarManager.m", "JTCalendar/JTCalendarDayView.m", "JTCalendar/JTCalendarDelegateManager.m", "JTCalendar/JTCalendarMenuView.m"],
             publicHeadersPath: "JTCalendar"),
     ]
 )
