098333e04c35a27033dac5d7ff4eb76e1221d84b
Konstantin Loginov <loginov.k@gmail.com>
2020-02-26 13:35:26 +0100

    Trying one more thing

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "JTCalendar",
-            path: "Sources"),
+            path: "Sources",
+            publicHeadersPath: "JTCalendar"),
     ]
 )
